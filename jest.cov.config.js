e9c7d87b0fe5db8fa928f52573433f7d3a16687a
Bing Ren <bingtimren@gmail.com>
2020-12-15 22:48:19 +1100

    Empty project, removed unused codes and scripts from starter

---
+++ jest.cov.config.js
@@ -0,0 +1,12 @@
+const baseConfig = require("./jest.config")
+module.exports = {
+    ...baseConfig,
+    coverageThreshold: {
+      global: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: -10
+      }
+    }  
+  };
